6701611938d4fb3861f94aec2056e1c8b252a1c7
sebasvega95 <sebas060495@hotmail.com>
2015-05-09 17:22:54 -0500

    Added some views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/logout', function(req, res, next) {
+  res.send('logout');
+});
+
+router.get('/account', function(req, res, next) {
+  res.send('account');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 module.exports = router;
